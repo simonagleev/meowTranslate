5cd4f86daf7b4a8a094916471006a5bc40df82a3
Simon <62551180+simonagleev@users.noreply.github.com>
2022-06-23 18:13:22 +0300

    created

--- main.js
+++ main.js
@@ -3,26 +3,12 @@ const toTextarea = document.querySelector('#to-textarea');
 const sapBtn = document.querySelector('.swap-button');
 const translateBtn = document.querySelector('.translate-button');
 
-const test = document.querySelector('.test');
 
-
-
-console.log(test)
-
-
-const butt = document.querySelector('#butt')
 
 translateBtn.addEventListener('click', function() {
   
-    const fromValue = test.value 
+    const fromValue = fromTextarea.value 
+
     console.log('test')
     console.log(fromValue)
 })
-let val = document.getElementById('elem1').value;
-butt.addEventListener('click', function() {
-
-    
-
-    document.getElementById('str').innerHTML="Вы ввели: " + val;
-}
-)
